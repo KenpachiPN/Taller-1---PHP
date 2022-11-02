b702dced6b4cbbb7e480e51b95e1135e6f5aea83
Santiago Prada Niño - ADSI <sprada71@misena.edu.co>
2022-11-02 12:18:22 -0500

    Avance Ejercicio 3

--- api.php
+++ api.php
@@ -4,68 +4,101 @@
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json; charset=utf-8');
     $_DATA = json_decode(file_get_contents("php://input"));
-class galeria{ 
-    public $codigo;
+// class galeria{ 
+//     public $codigo;
+//     public $lista;
+//     public $unidad;
+//     public function __construct(int $codigo, int $unidad){
+//         $this->codigo = $codigo;
+//         $this->unidad = $unidad;
+//         $this->lista = (object) [
+//             // Escribir un programa que permita ingresar para los N alumnos de una universidad:
+//             // SEXO (‘M’ o ‘F’), edad y carrera (‘A’,’B’,’C’). Imprimir la carrera con menor 
+//             // promedio de edad de sus alumnos que son varones
+//             111 =>  [
+//                 "Nombre" => "Camisa",
+//                 "Precio" => 50,
+//             ],
+//             222 =>  [
+//                 "Nombre" => "Pantalon",
+//                 "Precio" => 52,
+//             ],
+//             333 =>  [
+//                 "Nombre" => "Medias",
+//                 "Precio" => 45,
+//             ],
+//             444 =>  [
+//                 "Nombre" => "Gorra",
+//                 "Precio" => 85,
+//             ],
+//             555 =>  [
+//                 "Nombre" => "Buso",
+//                 "Precio" => 45,
+//             ],
+//             888 =>  [
+//                 "Nombre" => "Reloj",
+//                 "Precio" => 98,
+//             ],
+//             666 =>  [
+//                 "Nombre" => "Pulsera",
+//                 "Precio" => 832,
+//             ],
+//         ];
+//     }
+//     public function buscar():object{
+//         $res = (array) $this->lista;
+//         $men;
+//         if(array_key_exists($this->codigo, $res)){
+//             $men = (object) [
+//                 "Codigo" => $this->codigo,
+//                 "Producto" => $res[$this->codigo]["Nombre"],
+//                 "UnidadesCompradas" => $this->unidad,
+//                 "Total" => $res[$this->codigo]["Precio"] * $this->unidad,
+//             ];
+//         }else{
+//             $men = (object) [
+//                 "Codigo" => "Código inválido",
+//                 "Producto" => "Producto inválido",
+//                 "UnidadesCompradas" => 0,
+//                 "Total" => "Ingrese un código válido",
+//             ];
+//         }
+//         return $men;
+//     }
+// }
+// $obj = new galeria($_DATA->codigo, $_DATA->unidad);
+// echo json_encode(["Factura" => $obj->buscar()],JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+class universidad{ 
+    public $sexo;
+    public $edad;
     public $lista;
-    public $unidad;
-    public function __construct(int $codigo, int $unidad){
-        $this->codigo = $codigo;
-        $this->unidad = $unidad;
-        $this->lista = (object) [
-             // De la galería de productos, el usuario introducirá 
-             // el código y el número de unidades del producto que desea comprar.
-             //  El programa determinará el total a pagar, como una factura. Ejemplo de factura
-            111 =>  [
-                "Nombre" => "Camisa",
-                "Precio" => 50,
-            ],
-            222 =>  [
-                "Nombre" => "Pantalon",
-                "Precio" => 52,
-            ],
-            333 =>  [
-                "Nombre" => "Medias",
-                "Precio" => 45,
-            ],
-            444 =>  [
-                "Nombre" => "Gorra",
-                "Precio" => 85,
-            ],
-            555 =>  [
-                "Nombre" => "Buso",
-                "Precio" => 45,
-            ],
-            888 =>  [
-                "Nombre" => "Reloj",
-                "Precio" => 98,
-            ],
-            666 =>  [
-                "Nombre" => "Pulsera",
-                "Precio" => 832,
-            ],
-        ];
+    public $carrera;
+
+    public function __construct(string $sexo, string $carrera, int $edad){
+        $this->sexo = $sexo;
+        $this->carrera = $carrera;
+        $this->edad = $edad;
+        
+        // Escribir un programa que permita ingresar para los N alumnos de una universidad:
+        // SEXO (‘M’ o ‘F’), edad y carrera (‘A’,’B’,’C’). Imprimir la carrera con menor 
+        // promedio de edad de sus alumnos que son varones
+
+        
+        $data = json_decode(file_get_contents($this->carrera.".json"), true);
+        array_push($data, (object) [
+            "Universidad" =>  [
+                "Sexo" => $this->sexo,
+                "Edad" => $this->edad,
+            ]
+        ]);
+        $file = fopen($this->carrera.".json", "w+");
+        fwrite($file, json_encode($data, JSON_PRETTY_PRINT));
+        fclose($file);
+        
     }
-    public function buscar():object{
-        $res = (array) $this->lista;
-        $men;
-        if(array_key_exists($this->codigo, $res)){
-            $men = (object) [
-                "Codigo" => $this->codigo,
-                "Producto" => $res[$this->codigo]["Nombre"],
-                "UnidadesCompradas" => $this->unidad,
-                "Total" => $res[$this->codigo]["Precio"] * $this->unidad,
-            ];
-        }else{
-            $men = (object) [
-                "Codigo" => "Código inválido",
-                "Producto" => "Producto inválido",
-                "UnidadesCompradas" => 0,
-                "Total" => "Ingrese un código válido",
-            ];
-        }
-        return $men;
-    }
+
 }
-$obj = new galeria($_DATA->codigo, $_DATA->unidad);
-echo json_encode(["Factura" => $obj->buscar()],JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+ //https://www.php.net/manual/es/function.min.php
+$obj = new universidad($_DATA->sexo, $_DATA->carrera, $_DATA->edad);
+// echo json_encode(["Factura" => $obj->buscar()],JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 ?>
--- main.js
+++ main.js
@@ -4,6 +4,9 @@ addEventListener("DOMContentLoaded", (e) => {
     myForm.addEventListener("submit", async (e) => {
         e.preventDefault();
         if (e.submitter.dataset.operacion == "Limpiar") {
+            // Escribir un programa que permita ingresar para los N alumnos de una universidad: 
+            // SEXO (‘M’ o ‘F’), edad y carrera (‘A’,’B’,’C’). 
+            // Imprimir la carrera con menor promedio de edad de sus alumnos que son varones
             myForm.reset();
             document.querySelectorAll("tbody input").forEach(res => res.value="");
         } else {
@@ -13,11 +16,12 @@ addEventListener("DOMContentLoaded", (e) => {
                 body: JSON.stringify(data)
             };
             let peticion = await fetch(myForm.action, config);
-            let res = await peticion.json();
-            document.querySelector(`[name="codigoProducto"]`).value = res.Factura.Codigo;
-            document.querySelector(`[name="producto"]`).value = res.Factura.Producto;
-            document.querySelector(`[name="unidades"]`).value = res.Factura.UnidadesCompradas;
-            document.querySelector(`[name="total"]`).value = `$ ${res.Factura.Total}`;
+            let res = await peticion.text();
+            document.querySelector("pre").innerHTML = res;
+            // document.querySelector(`[name="codigoProducto"]`).value = res.Factura.Codigo;
+            // document.querySelector(`[name="producto"]`).value = res.Factura.Producto;
+            // document.querySelector(`[name="unidades"]`).value = res.Factura.UnidadesCompradas;
+            // document.querySelector(`[name="total"]`).value = `$ ${res.Factura.Total}`;
         }
     })
 })  
