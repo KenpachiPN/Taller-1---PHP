d6ddabbf358c653a5fa429a5f4bc45d520666a9a
Santiago Prada Niño - ADSI <sprada71@misena.edu.co>
2022-11-03 12:29:12 -0500

    Ejercicio 3

--- api.php
+++ api.php
@@ -1,104 +1,126 @@
-
 <?php
 
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json; charset=utf-8');
-    $_DATA = json_decode(file_get_contents("php://input"));
-// class galeria{ 
-//     public $codigo;
-//     public $lista;
-//     public $unidad;
-//     public function __construct(int $codigo, int $unidad){
-//         $this->codigo = $codigo;
-//         $this->unidad = $unidad;
-//         $this->lista = (object) [
-//             // Escribir un programa que permita ingresar para los N alumnos de una universidad:
-//             // SEXO (‘M’ o ‘F’), edad y carrera (‘A’,’B’,’C’). Imprimir la carrera con menor 
-//             // promedio de edad de sus alumnos que son varones
-//             111 =>  [
-//                 "Nombre" => "Camisa",
-//                 "Precio" => 50,
-//             ],
-//             222 =>  [
-//                 "Nombre" => "Pantalon",
-//                 "Precio" => 52,
-//             ],
-//             333 =>  [
-//                 "Nombre" => "Medias",
-//                 "Precio" => 45,
-//             ],
-//             444 =>  [
-//                 "Nombre" => "Gorra",
-//                 "Precio" => 85,
-//             ],
-//             555 =>  [
-//                 "Nombre" => "Buso",
-//                 "Precio" => 45,
-//             ],
-//             888 =>  [
-//                 "Nombre" => "Reloj",
-//                 "Precio" => 98,
-//             ],
-//             666 =>  [
-//                 "Nombre" => "Pulsera",
-//                 "Precio" => 832,
-//             ],
-//         ];
-//     }
-//     public function buscar():object{
-//         $res = (array) $this->lista;
-//         $men;
-//         if(array_key_exists($this->codigo, $res)){
-//             $men = (object) [
-//                 "Codigo" => $this->codigo,
-//                 "Producto" => $res[$this->codigo]["Nombre"],
-//                 "UnidadesCompradas" => $this->unidad,
-//                 "Total" => $res[$this->codigo]["Precio"] * $this->unidad,
-//             ];
-//         }else{
-//             $men = (object) [
-//                 "Codigo" => "Código inválido",
-//                 "Producto" => "Producto inválido",
-//                 "UnidadesCompradas" => 0,
-//                 "Total" => "Ingrese un código válido",
-//             ];
-//         }
-//         return $men;
-//     }
-// }
-// $obj = new galeria($_DATA->codigo, $_DATA->unidad);
-// echo json_encode(["Factura" => $obj->buscar()],JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
-class universidad{ 
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json; charset=utf-8');
+$_DATA = json_decode(file_get_contents("php://input"));
+$_DATA->accion = apache_request_headers()["accept"];
+
+class universidad
+{
     public $sexo;
     public $edad;
     public $lista;
     public $carrera;
 
-    public function __construct(string $sexo, string $carrera, int $edad){
+    public function __construct(string $sexo, string $carrera, int $edad)
+    {
         $this->sexo = $sexo;
         $this->carrera = $carrera;
         $this->edad = $edad;
-        
-        // Escribir un programa que permita ingresar para los N alumnos de una universidad:
-        // SEXO (‘M’ o ‘F’), edad y carrera (‘A’,’B’,’C’). Imprimir la carrera con menor 
-        // promedio de edad de sus alumnos que son varones
 
-        
-        $data = json_decode(file_get_contents($this->carrera.".json"), true);
-        array_push($data, (object) [
-            "Universidad" =>  [
-                "Sexo" => $this->sexo,
-                "Edad" => $this->edad,
+        $data = json_decode(file_get_contents($this->carrera . ".json"), true);
+        array_push(
+            $data,
+            (object) [
+                "Universidad" => [
+                    "Sexo" => $this->sexo,
+                    "Edad" => $this->edad,
+                ]
             ]
-        ]);
-        $file = fopen($this->carrera.".json", "w+");
+        );
+        $file = fopen($this->carrera . ".json", "w+");
         fwrite($file, json_encode($data, JSON_PRETTY_PRINT));
         fclose($file);
-        
+
+    }
+    public function Calcular()
+    {
+        $a = json_decode(file_get_contents("A.json"), true);
+        $b = json_decode(file_get_contents("B.json"), true);
+        $c = json_decode(file_get_contents("C.json"), true);
+        $totalAlumnosCarreras = count($a) + count($b) + count($c);
+        $alumnosA = count($a);
+        $alumnosB = count($b);
+        $alumnosC = count($c);
+
+
+        $sexoAlumnosA = array_column(array_column($a, 'Universidad'), 'Sexo');
+        $edadesVaronesA = 0;
+        $totalVaronesA = 0;
+        if ($alumnosA) {
+            foreach ($sexoAlumnosA as $key => $value) {
+                if ($a[$key]["Universidad"]["Sexo"] == "M") {
+                    $edadesVaronesA += $a[$key]["Universidad"]["Edad"];
+                    $totalVaronesA++;
+                }
+            }
+        }
+        $sexoAlumnosB = array_column(array_column($b, 'Universidad'), 'Sexo');
+        $edadesVaronesB = 0;
+        $totalVaronesB = 0;
+        if ($alumnosB) {
+            foreach ($sexoAlumnosB as $key => $value) {
+                if ($b[$key]["Universidad"]["Sexo"] == "M") {
+                    $edadesVaronesB += $b[$key]["Universidad"]["Edad"];
+                    $totalVaronesB++;
+                }
+            }
+        }
+        $sexoAlumnosC = array_column(array_column($c, 'Universidad'), 'Sexo');
+        $edadesVaronesC = 0;
+        $totalVaronesC = 0;
+        if ($alumnosC) {
+            foreach ($sexoAlumnosC as $key => $value) {
+                if ($c[$key]["Universidad"]["Sexo"] == "M") {
+                    $edadesVaronesC += $c[$key]["Universidad"]["Edad"];
+                    $totalVaronesC++;
+                }
+            }
+        }
+
+        $lista = (object) [
+            "A" => [
+                "Carrera" => "A",
+                "Total de edades de los varones" => $edadesVaronesA,
+                "Promedio de varones" => ($totalVaronesA) ? $edadesVaronesA / $totalVaronesA : 0,
+                "Promedio de varones por el total de alumnos" => $edadesVaronesA / ($alumnosA) ? $alumnosA : 0,
+                "Total de alumnos" => $alumnosA,
+                "Total de alumnos Varones" => $totalVaronesA
+            ],
+            "B" => [
+                "Carrera" => "B",
+                "Total de edades de los varones" => $edadesVaronesB,
+                "Promedio de varones" => ($totalVaronesB) ? $edadesVaronesB / $totalVaronesB : 0,
+                "Promedio de varones por el total de alumnos" => ($alumnosB) ? $edadesVaronesB / $alumnosB : 0,
+                "Total de alumnos" => $alumnosB,
+                "Total de alumnos Varones" => $totalVaronesB
+            ],
+            "C" => [
+                "Carrera" => "C",
+                "Total de edades de los varones" => $edadesVaronesC,
+                "Promedio de varones" => ($totalVaronesC) ? $edadesVaronesC / $totalVaronesC : 0,
+                "Promedio de varones por el total de alumnos" => ($alumnosC) ? $edadesVaronesC / $alumnosC : 0,
+                "Total de alumnos" => $alumnosC,
+                "Total de alumnos Varones" => $totalVaronesC
+            ]
+        ];
+        foreach (glob("*.json") as $file) {
+            $file = fopen($file, "w+");
+            fwrite($file, "[]");
+            fclose($file);
+        }
+        return $lista;
+
     }
 
 }
- //https://www.php.net/manual/es/function.min.php
-$obj = new universidad($_DATA->sexo, $_DATA->carrera, $_DATA->edad);
-// echo json_encode(["Factura" => $obj->buscar()],JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+
+if ($_DATA->accion == "Calcular") {
+    $obj = new universidad($_DATA->sexo, $_DATA->carrera, $_DATA->edad);
+    echo json_encode(["Promedios" => call_user_func_array([$obj, $_DATA->accion], [])], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+} else {
+    $obj = new universidad($_DATA->sexo, $_DATA->carrera, $_DATA->edad);
+}
+
+
 ?>
--- main.js
+++ main.js
@@ -3,25 +3,59 @@ addEventListener("DOMContentLoaded", (e) => {
     let myForm = document.querySelector("#formulario");
     myForm.addEventListener("submit", async (e) => {
         e.preventDefault();
+        let data = Object.fromEntries(new FormData(e.target));
+        let MyHeader = new Headers();
+
         if (e.submitter.dataset.operacion == "Limpiar") {
             // Escribir un programa que permita ingresar para los N alumnos de una universidad: 
             // SEXO (‘M’ o ‘F’), edad y carrera (‘A’,’B’,’C’). 
             // Imprimir la carrera con menor promedio de edad de sus alumnos que son varones
             myForm.reset();
-            document.querySelectorAll("tbody input").forEach(res => res.value="");
-        } else {
-            let data = Object.fromEntries(new FormData(e.target));
+            document.querySelectorAll("tbody input").forEach(res => res.value = "");
+        } else if (e.submitter.dataset.operacion == "Guardar") {
+            MyHeader.append("accept", false);
             let config = {
+                headers: MyHeader,
                 method: myForm.method,
                 body: JSON.stringify(data)
             };
             let peticion = await fetch(myForm.action, config);
             let res = await peticion.text();
-            document.querySelector("pre").innerHTML = res;
-            // document.querySelector(`[name="codigoProducto"]`).value = res.Factura.Codigo;
-            // document.querySelector(`[name="producto"]`).value = res.Factura.Producto;
-            // document.querySelector(`[name="unidades"]`).value = res.Factura.UnidadesCompradas;
-            // document.querySelector(`[name="total"]`).value = `$ ${res.Factura.Total}`;
+            // document.querySelector("pre").innerHTML = res;
+            myForm.reset();
+        }
+        else if (e.submitter.dataset.operacion == "Mostrar") {
+
+            MyHeader.append("Accept", "Calcular");
+            let config = {
+                headers: MyHeader,
+                method: myForm.method,
+                body: JSON.stringify(data)
+            };
+            let peticion = await fetch(myForm.action, config);
+            let res = await peticion.json();
+            // document.querySelector("pre").innerHTML = res;
+            //CARRERA A
+            document.querySelector(`[name="CarreraA"]`).value = res.Promedios.A["Carrera"];
+            document.querySelector(`[name="EdadVaronesA"]`).value = res.Promedios.A["Total de edades de los varones"];
+            document.querySelector(`[name="promVaronesA"]`).value = res.Promedios.A["Promedio de varones"];
+            document.querySelector(`[name="promVarAlumnA"]`).value = res.Promedios.A["Promedio de varones por el total de alumnos"];
+            document.querySelector(`[name="totalAlumnA"]`).value = res.Promedios.A["Total de alumnos"];
+            document.querySelector(`[name="totalAlumnVarA"]`).value = res.Promedios.A["Total de alumnos Varones"];
+            //CARRERA B
+            document.querySelector(`[name="CarreraB"]`).value = res.Promedios.B["Carrera"];
+            document.querySelector(`[name="EdadVaronesB"]`).value = res.Promedios.B["Total de edades de los varones"];
+            document.querySelector(`[name="promVaronesB"]`).value = res.Promedios.B["Promedio de varones"];
+            document.querySelector(`[name="promVarAlumnB"]`).value = res.Promedios.B["Promedio de varones por el total de alumnos"];
+            document.querySelector(`[name="totalAlumnB"]`).value = res.Promedios.B["Total de alumnos"];
+            document.querySelector(`[name="totalAlumnVarB"]`).value = res.Promedios.B["Total de alumnos Varones"];
+            //CARRERA C
+            document.querySelector(`[name="CarreraC"]`).value = res.Promedios.C["Carrera"];
+            document.querySelector(`[name="EdadVaronesC"]`).value = res.Promedios.C["Total de edades de los varones"];
+            document.querySelector(`[name="promVaronesC"]`).value = res.Promedios.C["Promedio de varones"];
+            document.querySelector(`[name="promVarAlumnC"]`).value = res.Promedios.C["Promedio de varones por el total de alumnos"];
+            document.querySelector(`[name="totalAlumnC"]`).value = res.Promedios.C["Total de alumnos"];
+            document.querySelector(`[name="totalAlumnVarC"]`).value = res.Promedios.C["Total de alumnos Varones"];
         }
     })
 })  
